980fc9dc09963428ed72f7aa7269bf95f7830b5e
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-02-22 12:55:06 +0200

    feat(api): adds swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Taraxa Claim Backend')
+    .setDescription('Taraxa Claim Backend')
+    .setVersion('1.0')
+    .addTag('claim')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apidocs', app, document);
+
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
