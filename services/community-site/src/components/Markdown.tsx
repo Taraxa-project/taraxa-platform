2b3b101fd776695cbf40552cb238dbb41668fe07
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-10-29 18:30:39 +0300

    adds Markdown component

---
+++ services/community-site/src/components/Markdown.tsx
@@ -0,0 +1,8 @@
+import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
+
+const Markdown = ({ children }: { children: string }) => {
+  return <ReactMarkdown remarkPlugins={[gfm]}>{children}</ReactMarkdown>;
+};
+
+export default Markdown;
