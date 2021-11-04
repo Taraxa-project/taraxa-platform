cf33059c5c201a8e73489a9a46853fbce9372df6
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-11-04 21:36:43 +0200

    adds proof of ownership to register node modal

--- services/community-site/src/pages/RunNode/Modal/RegisterNode.tsx
+++ services/community-site/src/pages/RunNode/Modal/RegisterNode.tsx
@@ -11,22 +11,59 @@ const RegisterNode = ({ onSuccess }: RegisterNodeProps) => {
   const api = useApi();
 
   const [nodePublicAddress, setNodePublicAddress] = useState('');
-  const [error, setError] = useState<string | undefined>(undefined);
+  const [signature, setSignature] = useState('');
+  const [errors, setErrors] = useState<{ key: string; value: string }[]>([]);
+
+  const errIndex = errors.map((error) => error.key);
+  const errValues = errors.map((error) => error.value);
+
+  const findErrorIndex = (field: string) => errIndex.findIndex((err) => err === field);
+  const hasError = (field: string) => findErrorIndex(field) !== -1;
+
+  const hasAddressError = hasError('address');
+  const addressErrorMessage = hasError('address') ? errValues[findErrorIndex('address')] : undefined;
+  const hasSignatureError = hasError('signature');
+  const signatureErrorMessage = hasError('signature')
+    ? errValues[findErrorIndex('signature')]
+    : undefined;
+
+  let hasGeneralError = false;
+  let generalErrorMessage = undefined;
+
+  if (errors.length > 0 && !hasAddressError && !hasSignatureError) {
+    hasGeneralError = true;
+    generalErrorMessage = errValues[0];
+  }
 
   const submit = async (
     event: React.MouseEvent<HTMLElement> | React.FormEvent<HTMLFormElement>,
   ) => {
     event.preventDefault();
 
-    setError(undefined);
+    setErrors([]);
 
-    const result = await api.post(`/nodes`, { ethWallet: nodePublicAddress }, true);
+    const result = await api.post(`/nodes`, { ethWallet: nodePublicAddress, sig: signature }, true);
     if (result.success) {
       onSuccess();
       return;
     }
 
-    setError(typeof result.response === 'string' ? result.response : undefined);
+    if (typeof result.response === 'string') {
+      if (result.response.search(/wallet/i) !== -1) {
+        setErrors([{ key: 'address', value: result.response }]);
+      } else if (result.response.search(/signature/i) !== -1) {
+        setErrors([{ key: 'signature', value: result.response }]);
+      } else {
+        setErrors([{ key: 'general', value: result.response }]);
+      }
+    } else {
+      setErrors(
+        result.response[0].messages.map((message: any) => ({
+          key: message.id.split('.')[3],
+          value: message.message,
+        })),
+      );
+    }
   };
 
   return (
@@ -45,8 +82,8 @@ const RegisterNode = ({ onSuccess }: RegisterNodeProps) => {
       <form onSubmit={submit}>
         <InputField
           label="Node public address"
-          error={error !== undefined}
-          helperText={error}
+          error={hasAddressError}
+          helperText={addressErrorMessage}
           value={nodePublicAddress}
           variant="outlined"
           type="text"
@@ -56,6 +93,22 @@ const RegisterNode = ({ onSuccess }: RegisterNodeProps) => {
             setNodePublicAddress(event.target.value);
           }}
         />
+        <InputField
+          label="Proof of node ownership"
+          error={hasSignatureError}
+          helperText={signatureErrorMessage}
+          value={signature}
+          variant="outlined"
+          type="text"
+          fullWidth
+          margin="normal"
+          onChange={(event) => {
+            setSignature(event.target.value);
+          }}
+        />
+
+        {hasGeneralError && <Text variant="body1" color="error">{generalErrorMessage!}</Text>}
+
         <Button
           type="submit"
           label="Submit"
@@ -70,13 +123,27 @@ const RegisterNode = ({ onSuccess }: RegisterNodeProps) => {
       <Text style={{ margin: '5% 0' }} label="References:" variant="body1" color="primary" />
 
       <Button
-        label="How to find my node's address"
+        label="How to find my node's address?"
         variant="outlined"
         color="secondary"
         className="node-control-reference-button"
         onClick={() =>
           window.open(
             `https://docs.taraxa.io/node-setup/node_address`,
+            '_blank',
+            'noreferrer noopener',
+          )
+        }
+        fullWidth={true}
+      />
+      <Button
+        label="How do I get the proof of owership?"
+        variant="outlined"
+        color="secondary"
+        className="node-control-reference-button"
+        onClick={() =>
+          window.open(
+            `https://docs.taraxa.io/node-setup/get-node-proof-of-owership`,
             '_blank',
             'noreferrer noopener',
           )
