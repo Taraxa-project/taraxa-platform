14c03ad86b0bbff6b528f1670f1db22fe73a0d17
Carlos Lage <carlos.lage@taraxa.io>
2021-11-16 13:16:50 +0000

    desktop styling for loader

--- services/community-site/src/components/Loader/Loader.tsx
+++ services/community-site/src/components/Loader/Loader.tsx
@@ -1,17 +1,21 @@
-// import { useMediaQuery } from 'react-responsive';
-import { Notification } from '@taraxa_project/taraxa-ui';
+import { useMediaQuery } from 'react-responsive';
 import { useLoading } from '../../services/useLoading';
+import { Box, CircularProgress } from '@material-ui/core';
 
 import './loader.scss';
 
 const Loader = () => {
   const { isLoading } = useLoading();
-  // const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
-  if (!isLoading) {
-    return null;
-  }
+  const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
+  const classeNames = [
+    isMobile ? 'mobile-loaderWidget' : 'desktop-loaderWidget',
+    isLoading ? 'show' : 'hide'
+  ].join(" ")
   
-  return <Notification variant="danger">Loading...</Notification>;
+  
+  return <Box id={"loaderWidget"} className={classeNames}>
+    <CircularProgress id="loaderWidgetProgress" size={35} thickness={4.5} /> Loading...
+  </Box>;
 };
 
 export default Loader;
