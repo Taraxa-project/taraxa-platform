92b73f1c4f0e8c91ee76e30d35c0e61b0da4bb94
Carlos Lage <carlos.lage@taraxa.io>
2021-11-18 14:03:22 +0000

    disable some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,23 @@ module.exports = {
       },
       extends: ['airbnb', 'airbnb/hooks', 'prettier', 'prettier/react'],
       plugins: ['prettier'],
+      rules: {
+        'jsx-a11y/anchor-is-valid': 'off',
+        'react/no-unescaped-entities': 'off',
+      },
     },
     {
       // Typescript and TSX
       // See https://github.com/toshi-toma/eslint-config-airbnb-typescript-prettier/blob/master/index.js
       files: ['*.{jsx,ts,tsx}'],
       extends: ['airbnb-typescript-prettier'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        'jsx-a11y/anchor-is-valid': 'off',
+        'react/no-unescaped-entities': 'off',
+      },
     },
   ],
   rules: {
