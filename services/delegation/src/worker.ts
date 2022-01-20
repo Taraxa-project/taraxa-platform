cfe290d39f858aa697791cb9cb28fd5f4afcd090
Leonard Mocanu <leonard.mocanu@ymail.com>
2022-01-20 18:53:16 +0200

    delegation: fixes cron and worker entrypoints

--- services/delegation/src/worker.ts
+++ services/delegation/src/worker.ts
@@ -2,9 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppCoreModule } from './modules/app-core.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(
-    AppCoreModule.forRoot('worker'),
-  );
-  await app.close();
+  await NestFactory.createApplicationContext(AppCoreModule.forRoot('worker'));
 }
 bootstrap();
