0168fe94a27ee8d3eb2a39ae648ca7f2ad52399f
Facundo Rebord <farebord@gmail.com>
2022-02-21 12:37:43 -0300

    removed console.log

--- services/community-site/src/pages/Delegation/NodeProfilePage.tsx
+++ services/community-site/src/pages/Delegation/NodeProfilePage.tsx
@@ -110,10 +110,6 @@ const NodeProfilePage = () => {
     (((node?.totalDelegation || 0) - (node?.ownDelegation || 0)) / delegationPossible) * 100;
   const selfDelegated = ((node?.ownDelegation || 0) / delegationPossible) * 100;
   const availableDelegation = ((node?.remainingDelegation || 0) / delegationPossible) * 100;
-
-  // eslint-disable-next-line no-console
-  console.log(communityDelegated, selfDelegated, availableDelegation);
-
   const delegationTotalPages = Math.ceil(delegationCount / 20);
   const offsetIndex = delegationPage === 1 ? 0 : 20 * (delegationPage - 1);
   const nodeActiveSince = new Date(node?.firstBlockCreatedAt || Date.now());
