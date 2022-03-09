19a6f204467078116f3bd27c49102d2b70f9ebfa
VargaElod23 <elod.varga@taraxa.io>
2022-03-10 00:08:24 +0200

    Finalize backend implementation

--- src/modules/claim/claim.service.ts
+++ src/modules/claim/claim.service.ts
@@ -146,13 +146,11 @@ export class ClaimService {
         ContractTypes.CLAIM,
         this.ethereumConfig.claimContractAddress,
       );
-
       const confirmation = await claimContractInstance.getClaimedAmount(
         address,
         claim.numberOfTokens,
         nonce,
       );
-
       if (
         confirmation.gt(ethers.BigNumber.from('0')) &&
         confirmation.eq(ethers.BigNumber.from(claim.numberOfTokens))
@@ -160,7 +158,6 @@ export class ClaimService {
         await this.markAsClaimed(claim.id);
       }
     }
-
     const account = JSON.parse(
       JSON.stringify(await this.accountRepository.findOneOrFail({ address })),
     );
