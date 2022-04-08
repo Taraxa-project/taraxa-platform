db0f93d798b1f7bc2c6d3aa29cae15bdfa479b6b
VargaElod23 <elod.varga@taraxa.io>
2022-04-08 17:42:54 +0300

    Fix redeem page BN comparisons

--- services/community-site/src/pages/Redeem/Redeem.tsx
+++ services/community-site/src/pages/Redeem/Redeem.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ethers } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
 import { BaseCard, Button, Loading, Notification } from '@taraxa_project/taraxa-ui';
 
 import {
@@ -68,7 +68,6 @@ function Redeem() {
           `${process.env.REACT_APP_API_CLAIM_HOST}/accounts/claims/${account}`,
           {},
         );
-
         if (claimData.success) {
           const finalClaims = redeem.formatClaimsForTable(
             claimData.response.data,
@@ -217,7 +216,9 @@ function Redeem() {
                       <TableCell className="tableCell">
                         {!row.claimed ? (
                           <Button
-                            disabled={availableToBeClaimed.toNumber() >= +row.numberOfTokens}
+                            disabled={availableToBeClaimed.lt(
+                              BigNumber.from(ethers.utils.parseUnits(row.numberOfTokens, 18)),
+                            )}
                             variant="outlined"
                             color="secondary"
                             onClick={() => onClaim(claims.indexOf(row))}
