309b3aed71f2fdf1fa073fe89767910f34bc270d
VargaElod23 <elod.varga@taraxa.io>
2022-05-03 19:56:52 +0300

    Implement warn modal

---
+++ services/community-site/src/pages/Redeem/Modal/Modals.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Modal } from '@taraxa_project/taraxa-ui';
+import { ethers } from 'ethers';
+import { formatEth, roundEth, weiToEth } from '../../../utils/eth';
+import CloseIcon from '../../../assets/icons/close';
+import RedeemWarning from './RedeemWarning';
+
+export interface RedeemModalsProps {
+  warningModal: boolean;
+  onWarningModalClose: () => void;
+  onWarningModalAccept: () => void;
+  taraAmount: ethers.BigNumber;
+}
+
+const RedeemModals = (props: RedeemModalsProps) => {
+  const { warningModal, onWarningModalClose, onWarningModalAccept, taraAmount } = props;
+  return (
+    <>
+      {warningModal && (
+        <Modal
+          id="warningModal"
+          title={`You are redeeming ${taraAmount} TARA`}
+          show={warningModal}
+          children={
+            <RedeemWarning
+              amount={formatEth(roundEth(weiToEth(taraAmount)))}
+              onDenial={onWarningModalClose}
+              onAccept={onWarningModalAccept}
+            />
+          }
+          parentElementID="root"
+          onRequestClose={() => onWarningModalClose()}
+          closeIcon={CloseIcon}
+        />
+      )}
+    </>
+  );
+};
+
+export default RedeemModals;
