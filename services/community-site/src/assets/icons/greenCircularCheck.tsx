21f8680cd3e3e192de473ad365b34602c896bc23
VargaElod23 <elod.varga@taraxa.io>
2022-05-13 16:56:14 +0300

    Add fill props to icons

--- services/community-site/src/assets/icons/greenCircularCheck.tsx
+++ services/community-site/src/assets/icons/greenCircularCheck.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 
-const GreenCircledCheckIcon = () => {
+interface CircleCheckProps {
+  color?: string;
+}
+
+const GreenCircledCheckIcon = (props: CircleCheckProps) => {
+  const { color } = props;
   return (
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z"
-        fill="#15AC5B"
+        fill={color || '#15AC5B'}
       />
     </svg>
   );
 };
 
-export const GreenCircledCheckIconBig = () => {
+export const GreenCircledCheckIconBig = (props: CircleCheckProps) => {
+  const { color } = props;
   return (
     <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M24 4C12.96 4 4 12.96 4 24C4 35.04 12.96 44 24 44C35.04 44 44 35.04 44 24C44 12.96 35.04 4 24 4ZM20 34L10 24L12.82 21.18L20 28.34L35.18 13.16L38 16L20 34Z"
-        fill="#15AC5B"
+        fill={color || '#15AC5B'}
       />
     </svg>
   );
