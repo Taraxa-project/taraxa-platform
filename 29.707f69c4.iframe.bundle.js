88bed5827f462902d4d3fa6f86b2f80a8340ccb4
VargaElod23 <100562086+VargaElod23@users.noreply.github.com>
2022-08-12 16:26:56 +0000

    Deploying to gh-pages from @ Taraxa-project/taraxa-platform@f052b416198394b6097c73a77394266aa135e5ea 🚀

---
+++ 29.707f69c4.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taraxa_project_taraxa_ui=self.webpackChunk_taraxa_project_taraxa_ui||[]).push([[29],{"../../node_modules/react-dom/client.js":(__unused_webpack_module,exports,__webpack_require__)=>{var m=__webpack_require__("../../node_modules/react-dom/index.js");exports.createRoot=m.createRoot,exports.hydrateRoot=m.hydrateRoot}}]);
