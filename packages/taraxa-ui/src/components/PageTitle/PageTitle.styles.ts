039944f75452b780476c6734155588dbe02a1732
VargaElod23 <elod.varga@taraxa.io>
2022-08-19 14:14:55 +0300

    Implement PageTitle component

---
+++ packages/taraxa-ui/src/components/PageTitle/PageTitle.styles.ts
@@ -0,0 +1,32 @@
+import { makeStyles } from '@mui/styles';
+import theme from '../theme';
+
+const useStyles = makeStyles(() => {
+  return {
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'left',
+      color: '#FFFFFF',
+      backgroundColor: '#151823',
+    },
+    title: {
+      letterSpacing: '-0.02em',
+      fontWeight: 500,
+      lineHeight: '2.75rem',
+      color: theme.palette.text.primary,
+      marginTop: '1rem',
+      marginBottom: '1rem',
+    },
+    subtitle: {
+      fontStyle: 'normal',
+      fontWeight: '400',
+      fontSize: '1rem',
+      lineHeight: '1.5rem',
+      color: theme.palette.text.secondary,
+      marginBottom: '1rem',
+    },
+  };
+});
+
+export default useStyles;
