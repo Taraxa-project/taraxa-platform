2fde6c17a9a2c24f49ab4b9975a7eef40603df80
VargaElod23 <elod.varga@taraxa.io>
2022-08-23 18:49:26 +0300

    Implement footer, add to app

--- services/explorer/src/App.tsx
+++ services/explorer/src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
+import Footer from './components/Footer';
 import logo from './logo.svg';
 
-function App() {
+declare global {
+  interface Window {
+    gtag: any;
+  }
+}
+
+const Root = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <Footer />
+    </>
   );
+};
+
+function App() {
+  return <Root />;
 }
 
 export default App;
---
+++ services/explorer/src/components/icons/index.ts
@@ -0,0 +1,6 @@
+import DiscordIcon from './discord';
+import SendIcon from './send';
+import TwitterIcon from './twitter';
+
+export { DiscordIcon, SendIcon, TwitterIcon };
+export default { DiscordIcon, SendIcon, TwitterIcon };
