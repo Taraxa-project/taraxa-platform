46749a369033c5f7547ebef76edaaaccb3eb2b8b
VargaElod23 <elod.varga@taraxa.io>
2022-08-23 18:50:19 +0300

    Fix header style

--- packages/taraxa-ui/src/components/theme.tsx
+++ packages/taraxa-ui/src/components/theme.tsx
@@ -67,11 +67,17 @@ const theme: Theme = createTheme({
           fontFamily: 'Inter',
           backgroundColor: '#282C3E',
           color: '#FFF',
+          minHeight: '4.5rem',
         },
       },
     },
     MuiToolbar: {
       styleOverrides: {
+        root: {
+          position: 'relative',
+          display: 'flex',
+          alignItems: 'center',
+        },
         regular: {
           minHeight: '72px',
           fontFamily: 'Inter',
@@ -98,10 +104,12 @@ const theme: Theme = createTheme({
           padding: '8px 16px',
         },
         sizeMedium: {
+          height: '52px',
           minWidth: '4rem',
           width: '100%',
         },
         outlinedSecondary: {
+          height: '52px',
           backgroundColor: '#31364B',
           borderColor: '#6A7085',
           color: '#FFF',
@@ -121,6 +129,7 @@ const theme: Theme = createTheme({
         },
         textSizeSmall: {
           height: '32px',
+          fontSize: '1rem',
           padding: '8px 16px',
         },
       },
