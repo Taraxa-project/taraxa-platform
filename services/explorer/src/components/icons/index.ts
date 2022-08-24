bfb3bc81fdea15dea24a216f73c7d5903b401831
VargaElod23 <elod.varga@taraxa.io>
2022-08-24 10:22:06 +0300

    Add header icons

--- services/explorer/src/components/icons/index.ts
+++ services/explorer/src/components/icons/index.ts
@@ -1,6 +1,8 @@
 import DiscordIcon from './discord';
 import SendIcon from './send';
 import TwitterIcon from './twitter';
+import HamburgerIcon from './hamburger';
+import TaraxaIcon from './taraxaIcon';
 
-export { DiscordIcon, SendIcon, TwitterIcon };
-export default { DiscordIcon, SendIcon, TwitterIcon };
+export { DiscordIcon, SendIcon, TwitterIcon, HamburgerIcon, TaraxaIcon };
+export default { DiscordIcon, SendIcon, TwitterIcon, HamburgerIcon, TaraxaIcon };
