69f49a06284869e74895ea7b6a6a301c0e6ccf33
VargaElod23 <elod.varga@taraxa.io>
2022-09-01 17:15:51 +0300

    Implement blockchain service for contract connectivity

---
+++ services/faucet/src/modules/blockchain/blockchain.service.ts
@@ -0,0 +1,47 @@
+import { ethers } from 'ethers';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { ethereum } from '@faucet/config';
+import { abi } from './contracts/Tara';
+
+export enum ContractTypes {
+  TARA,
+}
+
+@Injectable()
+export class BlockchainService {
+  private provider: ethers.providers.JsonRpcProvider;
+  public wallet: ethers.Wallet;
+
+  private contractInstances = {};
+
+  constructor(
+    @Inject(ethereum.KEY)
+    ethereumConfig: ConfigType<typeof ethereum>
+  ) {
+    this.provider = new ethers.providers.JsonRpcProvider({
+      url: ethereumConfig.provider,
+      timeout: 2000,
+    });
+    this.wallet = new ethers.Wallet(
+      ethereumConfig.privateSigningKey,
+      this.provider
+    );
+  }
+  getProvider() {
+    return this.provider;
+  }
+  getContractInstance(type: ContractTypes, address: string): ethers.Contract {
+    if (this.contractInstances[address]) {
+      return this.contractInstances[address];
+    }
+    switch (type) {
+      case ContractTypes.TARA:
+        return (this.contractInstances[address] = new ethers.Contract(
+          address,
+          abi,
+          this.wallet
+        ));
+    }
+  }
+}
