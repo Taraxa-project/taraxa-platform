e0cd8ffb95152700e39d5ed9541a864c99aa0d19
Florin Georgescu <flo_georgescu@hotmail.com>
2022-09-08 21:28:00 +0300

    feat(explorer): Started integrating grapql

--- services/explorer/src/index.tsx
+++ services/explorer/src/index.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
+import {
+  createClient as urqlCreatClient,
+  Provider as UrqlProvider,
+} from 'urql';
 import App from './App';
 import { ExplorerNetworkProvider } from './hooks/useExplorerNetwork';
 import reportWebVitals from './reportWebVitals';
 import { ExplorerThemeProvider } from './theme-provider';
 import { ExplorerLoaderProvider } from './hooks/useLoader';
+import { GRAPHQL_API } from './api';
+
+export const graphQLClient = urqlCreatClient({
+  url: GRAPHQL_API,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,11 +25,13 @@ root.render(
     <ExplorerThemeProvider>
       <ExplorerNetworkProvider>
         <ExplorerLoaderProvider>
-          <BrowserRouter>
-            <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
-              <App />
-            </SnackbarProvider>
-          </BrowserRouter>
+          <UrqlProvider value={graphQLClient}>
+            <BrowserRouter>
+              <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
+                <App />
+              </SnackbarProvider>
+            </BrowserRouter>
+          </UrqlProvider>
         </ExplorerLoaderProvider>
       </ExplorerNetworkProvider>
     </ExplorerThemeProvider>
