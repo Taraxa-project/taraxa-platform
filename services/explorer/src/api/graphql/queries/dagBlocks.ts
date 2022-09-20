4cac500f400796f057c6b5da4613d58fb1ceeeee
VargaElod23 <elod.varga@taraxa.io>
2022-09-20 19:15:58 +0300

    Add new fields to queries

--- services/explorer/src/api/graphql/queries/dagBlocks.ts
+++ services/explorer/src/api/graphql/queries/dagBlocks.ts
@@ -1,15 +1,54 @@
 export const dagBlockQuery = `
-  query dag_block_query($hash: Bytes32) {
-    dagBlock (hash: $hash) {
-      hash,
-      pivot,
-      tips,
-      level,
-      pbftPeriod,
-      timestamp,
-      transactionCount,
+query dag_block_query($hash: Bytes32) {
+  dagBlock (hash: "0c9d36dd14c50e0c319e095f06698a05bb80def51c7927d783514b051c619454") {
+    hash,
+    pivot,
+    tips,
+    level,
+    pbftPeriod,
+    timestamp,
+    transactionCount,
+    transactions {
+      hash
     }
   }
+}
+`;
+
+export const dagDetailsQuery = `
+query dag_details_query($hash: Bytes32) {
+  dagBlock(hash: $hash) {
+    hash
+    pivot
+    tips
+    level
+    pbftPeriod
+    timestamp
+    author {
+			address
+		}
+    transactionCount
+    vdf
+    signature
+    transactions{
+      hash
+      block {
+        number
+        timestamp
+      }
+      value
+      status
+      from{
+        address
+      }
+      to{
+        address
+      }
+      gasUsed
+      gasPrice
+    }
+  }
+}
 `;
 
 export const dagBlocksQuery = `
