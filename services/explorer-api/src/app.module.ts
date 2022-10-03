419e275fe419ea382c8316a563e054fe9be02f30
Florin Georgescu <flo_georgescu@hotmail.com>
2022-10-03 20:08:48 +0300

    feat(explorer-api): Initialized project

---
+++ services/explorer-api/src/app.module.ts
@@ -0,0 +1,68 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { resolve } from 'path';
+import { existsSync } from 'fs';
+
+const getEnvFilePath = () => {
+  const pathsToTest = ['../.env', '../../.env', '../../../.env'];
+
+  for (const pathToTest of pathsToTest) {
+    const resolvedPath = resolve(__dirname, pathToTest);
+
+    if (existsSync(resolvedPath)) {
+      return resolvedPath;
+    }
+  }
+};
+
+export const entities = [];
+
+const IndexerTypeOrmModule = () => {
+  let typeOrmOptions: TypeOrmModuleOptions;
+  const baseConnectionOptions: TypeOrmModuleOptions = process.env.DATABASE_URL
+    ? {
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        entities,
+        synchronize: false,
+        autoLoadEntities: true,
+        logging: ['info'],
+      }
+    : {
+        type: 'postgres',
+        host: process.env.DB_HOST ?? 'localhost',
+        port: Number(process.env.DB_PORT) || 5432,
+        username: process.env.DB_USERNAME || 'postgres',
+        password: process.env.DB_PASSWORD || 'postgres',
+        database: process.env.DB_DATABASE || 'hypepool',
+        entities,
+        synchronize: false,
+        autoLoadEntities: true,
+        logging: ['info'],
+      };
+
+  if (!!process.env.DATABASE_CERT) {
+    typeOrmOptions = {
+      ...baseConnectionOptions,
+      ssl: {
+        rejectUnauthorized: false,
+        ca: process.env.DATABASE_CERT,
+      },
+    };
+  } else {
+    typeOrmOptions = { ...baseConnectionOptions };
+  }
+  return TypeOrmModule.forRoot(typeOrmOptions);
+};
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: getEnvFilePath(),
+      isGlobal: true,
+    }),
+    IndexerTypeOrmModule(),
+  ],
+})
+export class AppModule {}
