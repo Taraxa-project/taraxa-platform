6c816f64ee0699a0e10ad6fe9fb69acca98e0b6c
Florin Georgescu <flo_georgescu@hotmail.com>
2022-10-04 13:25:21 +0300

    feat(explorer-indexer): Added new indexer service

---
+++ services/explorer-indexer/src/main.ts
@@ -0,0 +1,29 @@
+import 'reflect-metadata';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { useContainer } from 'class-validator';
+
+export function getPort(): number {
+  return parseInt(process.env.PORT || process.env.SERVER_PORT || '3000', 10);
+}
+
+async function bootstrap() {
+  const logger = new Logger('bootstrap');
+  const PORT = getPort();
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableShutdownHooks();
+  app.enableCors({
+    origin: '*',
+    exposedHeaders: ['Content-Type', 'Content-Range'],
+  });
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  await app.listen(PORT);
+  logger.log(`Application listening on port ${PORT}`);
+}
+bootstrap();
---
+++ services/explorer-indexer/src/modules/address/address.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Address } from './address.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Address])],
+  providers: [],
+  controllers: [],
+})
+export class AddressModule {}
