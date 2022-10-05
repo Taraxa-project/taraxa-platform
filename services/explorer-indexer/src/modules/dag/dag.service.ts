7d1cb486928ab2315f87034f66b06a81c2bdc86e
VargaElod23 <elod.varga@taraxa.io>
2022-10-05 19:19:45 +0300

    Adjust service code

--- services/explorer-indexer/src/modules/dag/dag.service.ts
+++ services/explorer-indexer/src/modules/dag/dag.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { IDAG } from '@taraxa_project/taraxa-models';
+import { NewDagBlockResponse, NewDagBlockFinalizedResponse } from 'src/types';
 import { Repository } from 'typeorm';
 import { DagEntity } from './dag.entity';
 
 @Injectable()
 export default class DagService {
-  private readonly logger: Logger = new Logger(DagService.name);
   constructor(
     @InjectRepository(DagEntity)
     private dagRepository: Repository<DagEntity>
@@ -14,7 +14,41 @@ export default class DagService {
     this.dagRepository = dagRepository;
   }
 
-  public handleNewDag(dag: IDAG) {
-    return this.dagRepository.save(dag);
+  public async handleNewDag(dagData: NewDagBlockResponse) {
+    const {
+      hash,
+      pivot,
+      tips,
+      level,
+      period,
+      timestamp,
+      author,
+      signature,
+      vdf,
+      transactionCount,
+      transactions,
+    } = { ...dagData };
+    const cherryPick: IDAG = {
+      hash,
+      pivot,
+      tips,
+      level,
+      pbftPeriod: period,
+      timestamp: parseInt(timestamp, 16),
+      author,
+      signature,
+      vdf,
+      transactionCount,
+      transactions,
+    };
+    return await this.dagRepository.save(cherryPick);
+  }
+
+  public async updateDag(updateData: NewDagBlockFinalizedResponse) {
+    const dag = await this.dagRepository.findOneBy({ hash: updateData.block });
+    if (dag) {
+      dag.pbftPeriod = parseInt(updateData.period, 10);
+      return await this.dagRepository.save(dag);
+    }
   }
 }
--- services/explorer-indexer/src/modules/pbft/pbft.service.ts
+++ services/explorer-indexer/src/modules/pbft/pbft.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { IPBFT } from '@taraxa_project/taraxa-models';
+import { NewPbftBlockResponse } from 'src/types';
 import { Repository } from 'typeorm';
 import { PbftEntity } from './pbft.entity';
 
 @Injectable()
 export default class PbftService {
-  private readonly logger: Logger = new Logger(PbftService.name);
   constructor(
     @InjectRepository(PbftEntity)
     private pbftRepository: Repository<PbftEntity>
@@ -14,7 +14,35 @@ export default class PbftService {
     this.pbftRepository = pbftRepository;
   }
 
-  public handleNewPbft(pbft: IPBFT) {
+  public handleNewPbft(pbftData: NewPbftBlockResponse) {
+    const {
+      hash,
+      number,
+      timestamp,
+      gasLimit,
+      gasUsed,
+      parent,
+      nonce,
+      difficulty,
+      totalDifficulty,
+      miner,
+      transactionCount,
+      transactions,
+    } = { ...pbftData };
+    const pbft: IPBFT = {
+      hash,
+      number: parseInt(number, 16),
+      timestamp: parseInt(timestamp, 16),
+      gasLimit: parseInt(gasLimit, 16),
+      gasUsed: parseInt(gasUsed, 16),
+      parent,
+      nonce,
+      difficulty: parseInt(difficulty, 16),
+      totalDifficulty: parseInt(totalDifficulty, 16),
+      miner,
+      transactionCount: parseInt(transactionCount, 16) || 0,
+      transactions,
+    };
     return this.pbftRepository.save(pbft);
   }
 }
