050f1b1c491b91cd6db41497448a2bce52878b40
Florin Georgescu <flo_georgescu@hotmail.com>
2022-10-21 20:42:23 +0300

    feat(explorer-indexer): Improved RPC response models

--- services/explorer-indexer/src/types/rpcWs/RpcWsResponse.interfaces.ts
+++ services/explorer-indexer/src/types/rpcWs/RpcWsResponse.interfaces.ts
@@ -1,39 +1,40 @@
+import { IDAG, IPBFT } from '@taraxa_project/explorer-shared';
+
 export interface NewDagBlockFinalizedResponse {
   block: string; //block hash
   period: string; // hex number of period of which the PBFT was created
 }
 
-export interface NewPbftBlockHeaderResponse {
-  hash: string;
+export interface NewPbftBlockHeaderResponse
+  extends Omit<
+    IPBFT,
+    | 'id'
+    | 'number'
+    | 'difficulty'
+    | 'totalDifficulty'
+    | 'transactionCount'
+    | 'transactions'
+    | 'timestamp'
+    | 'size'
+  > {
   number?: string; // hex number
   gas_limit?: string; // hex number
-  gasLimit?: string;
   gas_used?: string; // hex number
-  gasUsed?: string;
-  parent?: string;
-  nonce?: string; // hex number
   difficulty?: string; // hex number
   totalDifficulty?: string; // hex number
-  miner?: string;
   transactionCount?: string; // hex number
   transactions?: string[];
   author?: string; // block auther, same as miner
   extra_data?: string; // encoded extra data
-  extraData?: string;
   log_bloom?: string; // logs
-  logsBloom?: string;
-  mixHash?: string;
   parent_hash?: string;
   parentHash?: string;
   receipts_root?: string;
   receiptsRoot?: string;
-  sha3Uncles?: string;
   size?: string; // hex number
   state_root?: string;
-  stateRoot?: string;
   timestamp?: string; // hex number
   transactions_root?: string;
-  transactionsRoot?: string;
   uncles_hash?: string[];
   uncles?: string[];
   ethereum_rlp_size?: number;
@@ -56,21 +57,15 @@ export interface NewPbftBlockResponse {
   };
 }
 
-export interface NewDagBlockResponse {
-  hash: string;
-  pivot?: string;
-  tips?: string[];
-  level?: number;
+export interface NewDagBlockResponse
+  extends Omit<IDAG, 'pbftPeriod' | 'vdf' | 'timestamp' | 'transactions'> {
   period?: number;
-  signature?: string;
   sig?: string;
   vdf?: {
     difficulty: string;
     proof: string;
   };
-  transactionCount?: number;
   transactions?: string[];
-  author: string; // block author, same as miner
   sender: string; // block author, same as miner
   extraData: string; // encoded extra data
   logsBloom: string; // logs
