72f02efac3b66fe8c460c668e40ea40fc23794bd
Florin Georgescu <flo_georgescu@hotmail.com>
2022-10-21 20:33:35 +0300

    feat(explorer-indexer): Improved on GQL and RPC models

--- services/explorer-indexer/src/types/rpc/RPCPBFT.ts
+++ services/explorer-indexer/src/types/rpc/RPCPBFT.ts
@@ -1,23 +1,28 @@
-export interface RPCPbft {
+import { IPBFT } from '@taraxa_project/explorer-shared';
+
+export interface RPCPbft
+  extends Pick<
+    IPBFT,
+    | 'hash'
+    | 'gasLimit'
+    | 'gasUsed'
+    | 'extraData'
+    | 'logsBloom'
+    | 'mixHash'
+    | 'nonce'
+    | 'recepitsRoot'
+    | 'sha3Uncles'
+    | 'stateRoot'
+    | 'transactionsRoot'
+  > {
   author: string;
   difficulty: string; //hex
-  extraData: string;
-  gasLimit: string; //hex
-  gasUsed: string; //hex
-  hash: string;
-  logsBloom: string;
   miner: string;
-  mixHash: string;
-  nonce: string;
   number: string; //hex
   parentHash: string;
-  recepitsRoot: string;
-  sha3Uncles: string;
   size: string; //hex
-  stateRoot: string;
   timestamp: string; //hex
   totalDifficulty: string; //hex
-  transactionsRoot: string;
   uncles: string[];
   transactions: string[];
 }
