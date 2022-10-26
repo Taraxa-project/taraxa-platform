a529f26dcb95cfb907b6bf760ac19ecba7adbc63
VargaElod23 <elod.varga@taraxa.io>
2022-10-26 13:27:11 +0300

    Add bull board to monitor queues

---
+++ services/explorer-indexer/src/bullBoard.ts
@@ -0,0 +1,18 @@
+import { ExpressAdapter } from '@bull-board/express';
+import { INestApplication } from '@nestjs/common';
+import { createBullBoard } from '@bull-board/api';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
+import { Queue } from 'bull';
+
+export const initializeBullBoard = (app: INestApplication) => {
+  const serverAdapter = new ExpressAdapter();
+  serverAdapter.setBasePath('/bull-board');
+
+  const pbftQueue = app.get<Queue>(`BullQueue_new_pbfts`);
+  const dagsQueue = app.get<Queue>(`BullQueue_new_dags`);
+
+  createBullBoard({
+    queues: [new BullAdapter(pbftQueue), new BullAdapter(dagsQueue)],
+    serverAdapter,
+  });
+};
--- services/explorer-indexer/src/main.ts
+++ services/explorer-indexer/src/main.ts
@@ -1,9 +1,22 @@
+import expressBasicAuth from 'express-basic-auth';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { initializeBullBoard } from './bullBoard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const serverAdapter = initializeBullBoard(app);
 
+  app.use(
+    '/bull-board',
+    expressBasicAuth({
+      users: {
+        user: 'password',
+      },
+      challenge: true,
+    }),
+    serverAdapter.getRouter()
+  );
   await app.listen(process.env.SERVER_PORT || 3040);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
