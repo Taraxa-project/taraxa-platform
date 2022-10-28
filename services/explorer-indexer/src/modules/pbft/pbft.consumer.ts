79b07ce67cb2f0e3fcf291de1642be583e9d1626
VargaElod23 <elod.varga@taraxa.io>
2022-10-28 18:17:36 +0300

    handle concurrency issues with multiple workers on pbft save

--- services/explorer-indexer/src/modules/pbft/pbft.consumer.ts
+++ services/explorer-indexer/src/modules/pbft/pbft.consumer.ts
@@ -15,7 +15,9 @@ export class PbftConsumer implements OnModuleInit {
     private pbftService: PbftService,
     private readonly graphQLConnector: GraphQLConnectorService,
     @InjectQueue(Queues.NEW_DAGS)
-    private readonly dagsQueue: Queue
+    private readonly dagsQueue: Queue,
+    @InjectQueue(Queues.NEW_PBFTS)
+    private readonly pbftsQueue: Queue
   ) {}
   onModuleInit() {
     this.logger.debug(`Init ${PbftConsumer.name} worker`);
@@ -39,36 +41,46 @@ export class PbftConsumer implements OnModuleInit {
     }
     const newBlock: IGQLPBFT =
       await this.graphQLConnector.getPBFTBlockForNumber(pbftPeriod);
-
-    if (newBlock && newBlock.number != undefined) {
-      const formattedBlock: IPBFT = this.pbftService.pbftGQLToIPBFT(newBlock);
-      if (type === SyncTypes.LIVE) {
-        await this.pbftService.checkAndDeletePbftsGreaterThanNumber(
-          newBlock.number
+    try {
+      if (newBlock && newBlock.number != undefined) {
+        const formattedBlock: IPBFT = this.pbftService.pbftGQLToIPBFT(newBlock);
+        if (type === SyncTypes.LIVE) {
+          await this.pbftService.checkAndDeletePbftsGreaterThanNumber(
+            newBlock.number
+          );
+        }
+        this.logger.debug(
+          `${QueueJobs.NEW_PBFT_BLOCKS} worker (job ${job.id}): Saving PBFT ${job.data.pbftPeriod}`
         );
-      }
-      this.logger.debug(
-        `${QueueJobs.NEW_PBFT_BLOCKS} worker (job ${job.id}): Saving PBFT ${job.data.pbftPeriod}`
-      );
-      let blockReward = new BigInteger('0', 10);
-      formattedBlock.transactions?.forEach((tx: ITransaction) => {
-        const gasUsed = new BigInteger(tx.gasUsed.toString() || '0');
-        tx.gasUsed = gasUsed.toString();
-        const gasPrice = new BigInteger(tx.gasPrice.toString() || '0');
-        tx.gasPrice = gasPrice.toString();
-        const reward = gasUsed.multiply(gasPrice);
-        const newVal = blockReward.add(reward);
-        blockReward = newVal;
-      });
+        let blockReward = new BigInteger('0', 10);
+        formattedBlock.transactions?.forEach((tx: ITransaction) => {
+          const gasUsed = new BigInteger(tx.gasUsed.toString() || '0');
+          tx.gasUsed = gasUsed.toString();
+          const gasPrice = new BigInteger(tx.gasPrice.toString() || '0');
+          tx.gasPrice = gasPrice.toString();
+          const reward = gasUsed.multiply(gasPrice);
+          const newVal = blockReward.add(reward);
+          blockReward = newVal;
+        });
 
-      formattedBlock.reward = blockReward.toString();
-      const savedPbft = await this.pbftService.safeSavePbft(formattedBlock);
-      this.logger.debug(
-        `Saved new PBFT with ID ${savedPbft.id} for period ${savedPbft.number}`
+        formattedBlock.reward = blockReward.toString();
+        const savedPbft = await this.pbftService.safeSavePbft(formattedBlock);
+        this.logger.debug(
+          `Saved new PBFT with ID ${savedPbft.id} for period ${savedPbft.number}`
+        );
+        this.dagsQueue.add(QueueJobs.NEW_DAG_BLOCKS, {
+          pbftPeriod: savedPbft.number,
+        });
+      }
+    } catch (error) {
+      this.logger.error(
+        `An error occurred during saving PBFT ${pbftPeriod}. Putting it back to queue: `,
+        error
       );
-      this.dagsQueue.add(QueueJobs.NEW_DAG_BLOCKS, {
-        pbftPeriod: savedPbft.number,
-      });
+      this.pbftsQueue.add(QueueJobs.NEW_PBFT_BLOCKS, {
+        pbftPeriod,
+        type,
+      } as QueueData);
     }
   }
 }
