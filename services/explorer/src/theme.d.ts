9a5da489bf6ec3a6908eb6ef7a16088db5d89329
VargaElod23 <elod.varga@taraxa.io>
2022-12-22 16:36:51 +0200

    change added attribute to optional

--- services/explorer/src/theme.d.ts
+++ services/explorer/src/theme.d.ts
@@ -4,6 +4,6 @@ declare module '@mui/styles' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface DefaultTheme extends Theme {
     // just so it isn't empty
-    isEmpty: false;
+    isEmpty?: false;
   }
 }
