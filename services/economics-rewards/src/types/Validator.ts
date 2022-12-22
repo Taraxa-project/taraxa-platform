2298f25f33aca2e0e466ee89c821a146d117ea23
Florin Georgescu <flo_georgescu@hotmail.com>
2022-12-22 17:57:01 +0200

    chore(economics): Added comments to interface props

--- services/economics-rewards/src/types/Validator.ts
+++ services/economics-rewards/src/types/Validator.ts
@@ -1,10 +1,17 @@
 export interface ValidatorBasicInfo {
+  // Total number of delegated tokens to the validator
   total_stake: string;
+  // Validator's reward from delegators rewards commission
   commission_reward: string;
+  // Validator's commission - max value 1000(precision up to 0.1%)
   commission: string;
+  // Block number of last commission change
   last_commission_change: string;
+  // Validator's owner account
   owner: string;
+  // Validators description/name
   description: string;
+  // Validators website endpoint
   endpoint: string;
 }
 
