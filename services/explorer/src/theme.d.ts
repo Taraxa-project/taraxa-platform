6fa8608a2bfd2680f62c6613fe3bba163cae3dc6
Florin Georgescu <flo_georgescu@hotmail.com>
2022-12-23 13:38:41 +0200

    chore: Updated empty interface

--- services/explorer/src/theme.d.ts
+++ services/explorer/src/theme.d.ts
@@ -1,6 +1,8 @@
 import { Theme } from '@mui/material';
 
 declare module '@mui/styles' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
+  interface DefaultTheme extends Theme {
+    // just so it isn't empty
+    isEmpty?: false;
+  }
 }
