df42e6c3000ac50b9aba0060338a8cf87e09ebc6
Leonard Mocanu <leonard.mocanu@ymail.com>
2023-01-17 17:41:16 +0200

    explorer-indexer: changes env var name

--- services/explorer-indexer/src/config/general.ts
+++ services/explorer-indexer/src/config/general.ts
@@ -13,6 +13,6 @@ export default registerAs('general', () => {
     redisPassword: process.env.QUEUE_PASS,
     isProducer: process.env.ENABLE_PRODUCER_MODULE,
     reorgThreshold: Number(process.env.REORG_THRESHOLD),
-    maxLockDuration: Number(process.env.REDIS_MAX_LOCK_DURATION || 30000),
+    maxLockDuration: Number(process.env.QUEUE_MAX_LOCK_DURATION || 30000),
   };
 });
