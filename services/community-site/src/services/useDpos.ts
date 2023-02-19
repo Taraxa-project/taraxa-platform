5bd45da864b754c4a8cfa85f3709929d3b287bde
Florin Georgescu <flo_georgescu@hotmail.com>
2023-02-19 13:24:46 +0200

    fix(community): Adjusted cards on table view

--- services/community-site/src/services/useDpos.ts
+++ services/community-site/src/services/useDpos.ts
@@ -25,6 +25,7 @@ function useDpos() {
     'function setCommission(address validator, uint16 commission)',
     'function setValidatorInfo(address validator, string description, string endpoint)',
     'function undelegate(address validator, uint256 amount)',
+    'function getValidatorsFor(address owner, uint256 batch)',
   ];
 
   const mainnetDpos = useMemo(() => {
