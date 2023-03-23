de46af2f6acd187e4b2db19a89a7b448466d972a
Florin Georgescu <flo_georgescu@hotmail.com>
2023-03-23 13:24:51 +0200

    Fix/explorer network redirect (#418)
    * Fixed environment network selection
    
    * Fixing infinite loop on qa redirect
    
    * Checking domains and subdoamins before redirecting

--- services/explorer/src/utils/Enums.ts
+++ services/explorer/src/utils/Enums.ts
@@ -15,19 +15,13 @@ export enum HashLinkType {
 export const SELECTED_NETWORK = 'SELECTED_NETWORK';
 
 export enum Network {
+  MAINNET = 'Mainnet Candidate',
   TESTNET = 'Californicum Testnet',
   DEVNET = 'Californicum Devnet',
-  MAINNET = 'Mainnet Candidate',
 }
 
 export enum NetworkGraphQLEndpoints {
   MAINNET = 'https://graphql.mainnet.taraxa.io/',
   TESTNET = 'https://graphql.testnet.taraxa.io/',
   DEVNET = 'https://graphql.devnet.taraxa.io/',
-}
-
-export enum ENVIRONMENT {
-  LOCALHOST = 'localhost',
-  QA = 'qa',
-  PROD = 'prod',
 }
