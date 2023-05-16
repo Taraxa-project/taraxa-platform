07fb9e8cdddd15ea7306b568d3ebf0ef07b8fc5b
Előd Varga <100562086+VargaElod23@users.noreply.github.com>
2023-05-16 16:36:13 +0300

    Feature/add subgraph for dpos (#390)
    * adds subgraph for DPOS contract
    
    * refactor enetities
    
    * implements handlers for simplified arch
    
    * fixes wasm compilation
    
    * removes block trigger
    
    * simplfies data: elimiantes delegator and validator info
    
    * finalizes first iteration
    
    * removes commented code
    
    * finalizes first version
    
    * implements current delegations
    
    * adds failfast on deleted validators
    
    * removes unncecessary reassignments
    
    * removes duplicate falsy check
    
    * specifies o-t-m as derived
    
    * uses stake update method for undelegation calc
    
    * adds logic for validator removal on undelegate
    
    * removes validator entity
    
    * adds dates to delegations and commission changes, cleans up PR review
    
    * fixes date set with block ts instead of number

---
+++ packages/dpos-subgraph/src/mapping.ts
@@ -0,0 +1,93 @@
+import { Address, BigInt, log } from '@graphprotocol/graph-ts';
+import {
+  CommissionSet,
+  Delegated,
+  Redelegated,
+  UndelegateCanceled,
+  Undelegated,
+} from '../generated/DPOS/DPOS';
+import { CommissionChange, Delegation } from '../generated/schema';
+
+function getOrInitCurrentDelegation(
+  validator: Address,
+  delegator: Address
+): Delegation {
+  const delegationId = `${validator.toHexString()}-${delegator.toHexString()}`;
+  let delegation = Delegation.load(delegationId);
+  if (!delegation) {
+    log.debug(`Delegation ${delegationId} not found in store. Creating...`, [
+      delegationId,
+    ]);
+    delegation = new Delegation(delegationId);
+    delegation.validator = validator.toHexString();
+    delegation.delegator = delegator.toHexString();
+    delegation.amount = BigInt.zero();
+    delegation.date = BigInt.zero();
+    delegation.save();
+  }
+  return delegation;
+}
+
+export function handleDelegated(event: Delegated): void {
+  const delegator = event.params.delegator;
+  const validator = event.params.validator;
+  const amount = event.params.amount;
+  log.debug('Handling delegation' + amount.toString(), [amount.toString()]);
+  const newCurrentDelegation = getOrInitCurrentDelegation(validator, delegator);
+  newCurrentDelegation.amount = newCurrentDelegation.amount.plus(amount);
+  newCurrentDelegation.date = event.block.timestamp;
+  newCurrentDelegation.save();
+}
+
+export function handleUndelegated(event: Undelegated): void {
+  const delegator = event.params.delegator;
+  const validator = event.params.validator;
+  const amount = event.params.amount;
+  const currentDelegation = getOrInitCurrentDelegation(validator, delegator);
+  currentDelegation.amount = currentDelegation.amount.minus(amount);
+  currentDelegation.save();
+}
+
+export function handleUndelegateCanceled(event: UndelegateCanceled): void {
+  const delegator = event.params.delegator;
+  const validator = event.params.validator;
+
+  const currentDelegation = getOrInitCurrentDelegation(validator, delegator);
+  currentDelegation.amount = currentDelegation.amount.plus(event.params.amount);
+  currentDelegation.date = event.block.timestamp;
+  currentDelegation.save();
+}
+
+export function handleRedelegated(event: Redelegated): void {
+  const delegator = event.params.delegator;
+  const from = event.params.from;
+  const to = event.params.to;
+  const amount = event.params.amount;
+
+  // Create delegations
+  const currentDelegationDataTo = getOrInitCurrentDelegation(to, delegator);
+  const currentDelegationDataFrom = getOrInitCurrentDelegation(from, delegator);
+
+  // Update delegations
+  currentDelegationDataFrom.amount =
+    currentDelegationDataFrom.amount.minus(amount);
+  currentDelegationDataFrom.save();
+
+  currentDelegationDataTo.amount = currentDelegationDataTo.amount.plus(amount);
+  currentDelegationDataTo.date = event.block.timestamp;
+  currentDelegationDataTo.save();
+}
+
+export function handleCommissionSet(event: CommissionSet): void {
+  const commission = event.params.commission;
+  const validator = event.params.validator;
+
+  const commissionChange = new CommissionChange(event.block.hash.toHexString());
+  commissionChange.commission = commission;
+  commissionChange.validator = validator.toHexString();
+  commissionChange.registrationBlock = event.block.number.toI32();
+  const appliance = event.block.number.plus(BigInt.fromI32(25000));
+  commissionChange.applianceBlock = appliance.toI32();
+  commissionChange.date = event.block.timestamp;
+  commissionChange.save();
+}
