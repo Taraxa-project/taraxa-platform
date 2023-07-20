8128d875be3dd5567c2881bc55e7c4e82964a568
Florin Georgescu <flo_georgescu@hotmail.com>
2023-07-20 16:24:43 +0300

    Simplified progress bar

--- packages/taraxa-ui/src/components/ProgressBar/ProgressBar.styles.ts
+++ packages/taraxa-ui/src/components/ProgressBar/ProgressBar.styles.ts
@@ -1,68 +1,20 @@
 import { makeStyles } from '@mui/styles';
 import theme from '../theme';
 
-export interface ProgressBarCellProps {
-  percentage: number;
-}
-
 const useStyles = makeStyles(() => {
   return {
-    barFlex: {
-      display: 'flex',
-      flexDirection: 'row',
-      marginBottom: '4px',
-      paddingTop: '2rem',
-      percentageAmount: {
-        display: 'flex',
-        flexDirection: 'column',
-      },
-      barPercentage: {
-        height: '0.25rem',
-        width: '100%',
-      },
-    },
-    percentageAmount: {
-      '&:first-child': {
-        barPercentage: {
-          borderTopLeftRadius: '2px',
-          borderBottomLeftRadius: '2px',
-        },
-      },
-      '&:last-child': {
-        barPercentage: {
-          borderTopRightRadius: '2px',
-          borderBottomRightRadius: '2px',
-        },
-      },
-    },
     cellContainer: {
       position: 'relative',
     },
     percentageText: {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
+      textAlign: 'center',
+      margin: '0 auto',
     },
     progressBar: {
-      position: 'absolute',
-      bottom: '0',
-      height: '0.5rem',
-      backgroundColor: theme.palette.secondary.main,
-    },
-    greyBar: {
-      position: 'absolute',
-      bottom: '0',
-      height: '0.5rem',
-      backgroundColor: theme.palette.background.default,
-    },
-    barPercentage: {
-      height: '0.25rem',
       width: '100%',
-      borderTopRightRadius: '2px',
-      borderBottomRightRadius: '2px',
-      borderTopLeftRadius: '2px',
-      borderBottomLeftRadius: '2px',
+      borderRadius: '2px',
+      overflow: 'hidden',
+      height: '0.25rem',
       backgroundColor: theme.palette.grey[300],
     },
   };
--- packages/taraxa-ui/src/components/ProgressBar/ProgressBar.tsx
+++ packages/taraxa-ui/src/components/ProgressBar/ProgressBar.tsx
@@ -1,32 +1,24 @@
 import React from 'react';
-import useStyles, { ProgressBarCellProps } from './ProgressBar.styles';
+import useStyles from './ProgressBar.styles';
 import theme from '../theme';
+
+export interface ProgressBarCellProps {
+  percentage: number;
+}
 
 const ProgressBar: React.FC<ProgressBarCellProps> = ({ percentage }) => {
   const classes = useStyles();
   return (
     <div className={classes.cellContainer}>
-      <span className={classes.percentageText}>{percentage}%</span>
-      <div className={classes.barFlex}>
+      <p className={classes.percentageText}>{percentage}%</p>
+      <div className={classes.progressBar}>
         <div
-          className={classes.percentageAmount}
           style={{
-            width: `${Math.max(percentage, 1)}%`,
+            width: `${percentage}%`,
+            backgroundColor: theme.palette.secondary.main,
+            height: '100%',
           }}
-        >
-          <div
-            className={classes.barPercentage}
-            style={{ background: theme.palette.secondary.main }}
-          />
-        </div>
-        <div
-          className={classes.percentageAmount}
-          style={{
-            width: `${100 - percentage}%`,
-          }}
-        >
-          <div className={classes.barPercentage} />
-        </div>
+        />
       </div>
     </div>
   );
