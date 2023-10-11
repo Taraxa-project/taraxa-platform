5ff7a49dc30c7e1e3c2cf51d618dd760da882cd4
Florin Georgescu <flo_georgescu@hotmail.com>
2023-10-11 15:51:30 +0300

    Fixing ui build issues

--- packages/taraxa-sdk/rollup.config.js
+++ packages/taraxa-sdk/rollup.config.js
@@ -2,7 +2,6 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
-import postcss from 'rollup-plugin-postcss';
 import url from '@rollup/plugin-url';
 import image from '@rollup/plugin-image';
 
@@ -28,9 +27,6 @@ export default {
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
-    postcss({
-      extensions: ['.css'],
-    }),
     url({
       include: ['**/*.woff', '**/*.woff2', '**/*.eot', '**/*.ttf'],
       name: '[name].[ext]',
