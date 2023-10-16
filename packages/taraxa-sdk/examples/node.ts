64f2f92c7d2e3ca4be25a448d435dc0abaeb8df3
Florin Georgescu <flo_georgescu@hotmail.com>
2023-10-16 12:59:55 +0300

    Removed private key example

--- packages/taraxa-sdk/examples/node.ts
+++ packages/taraxa-sdk/examples/node.ts
@@ -3,8 +3,7 @@ import { DposClient } from '../src/DposClient';
 
 const runExample = async () => {
   const networkName = NetworkName.MAINNET;
-  const privateKey =
-    'aae3250deafff925811a14af4d7d0c9ba88b234c9f98443f9a203231bd36e805'; // Replace with your private key
+  const privateKey = 'your-private-key'; // Replace with your private key
 
   const client = new DposClient(networkName, privateKey);
 
