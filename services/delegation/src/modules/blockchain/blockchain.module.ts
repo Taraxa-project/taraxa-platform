4e2d9001ddcb47a341d35efea9a0c3c5f57e2bd3
Leonard Mocanu <leonard.mocanu@ymail.com>
2024-10-14 18:20:43 +0300

    delegation: changes default commission for node creation

--- services/delegation/src/modules/blockchain/blockchain.module.ts
+++ services/delegation/src/modules/blockchain/blockchain.module.ts
@@ -22,6 +22,7 @@ const getProviders = () => [
         ethereum.testnetWallet,
         delegation.testnetDelegation,
         delegation.testnetOwnNodes,
+        10000,
       );
     },
     inject: [delegationConfig.KEY, ethereumConfig.KEY],
@@ -37,6 +38,7 @@ const getProviders = () => [
         ethereum.mainnetWallet,
         delegation.minDelegation,
         [],
+        0,
       );
     },
     inject: [delegationConfig.KEY, ethereumConfig.KEY],
